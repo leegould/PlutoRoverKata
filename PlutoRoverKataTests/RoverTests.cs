3e90e7ca0245d45aa0c30be8a9c048f37d198571
Lee <gould.lee@googlemail.com>
2016-01-28 21:47:16 +0000

    First test

---
+++ PlutoRoverKataTests/RoverTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PlutoRoverKataTests
+{
+    public class RoverTests
+    {
+        [Test]
+        public void Command_Forward_Valid()
+        {
+            var rover = new Rover(0, 0, 'N');
+
+            rover.Command('F');
+             
+            Assert.AreEqual(0, rover.X);
+            Assert.AreEqual(1, rover.Y);
+            Assert.AreEqual('N', rover.Heading);
+        }
+    }
+}
