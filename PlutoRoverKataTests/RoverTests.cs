5ad91fdba24a01be509109101152475af6bc7cf3
Lee <gould.lee@googlemail.com>
2016-01-28 22:29:18 +0000

    Adding some classes to test to split them up a bit.

--- PlutoRoverKataTests/RoverTests.cs
+++ PlutoRoverKataTests/RoverTests.cs
@@ -10,186 +10,195 @@ namespace PlutoRoverKataTests
 {
     public class RoverTests
     {
-        [Test]
-        public void Constructor_Valid()
+        public class ConstructorTests
         {
-            var rover = new Rover(0, 0, 'N');
+            [Test]
+            public void Constructor_Valid()
+            {
+                var rover = new Rover(0, 0, 'N');
 
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(0, rover.Y);
-            Assert.AreEqual('N', rover.Heading);
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(0, rover.Y);
+                Assert.AreEqual('N', rover.Heading);
+            }
         }
 
-        [Test]
-        public void Command_Forward_Valid()
+        public class MovementTests
         {
-            var rover = new Rover(0, 0, 'N');
+            [Test]
+            public void Command_Forward_Valid()
+            {
+                var rover = new Rover(0, 0, 'N');
 
-            rover.Command('F');
+                rover.Command('F');
 
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(1, rover.Y);
-            Assert.AreEqual('N', rover.Heading);
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(1, rover.Y);
+                Assert.AreEqual('N', rover.Heading);
+            }
+
+            [Test]
+            public void Command_Backward_Valid()
+            {
+                var rover = new Rover(0, 0, 'N');
+
+                rover.Command('B');
+
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(-1, rover.Y);
+                Assert.AreEqual('N', rover.Heading);
+            }
+
+            [Test]
+            public void Command_ForwardTwice_Valid()
+            {
+                var rover = new Rover(0, 0, 'N');
+
+                rover.Command('F');
+                rover.Command('F');
+
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(2, rover.Y);
+                Assert.AreEqual('N', rover.Heading);
+            }
+
+            [Test]
+            public void Command_ForwardThenBackward_Valid()
+            {
+                var rover = new Rover(0, 0, 'N');
+
+                rover.Command('F');
+                rover.Command('B');
+
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(0, rover.Y);
+                Assert.AreEqual('N', rover.Heading);
+            }
         }
 
-        [Test]
-        public void Command_Backward_Valid()
+        public class HeadingTests
         {
-            var rover = new Rover(0, 0, 'N');
+            [Test]
+            public void Command_Turn_Valid()
+            {
+                var rover = new Rover(0, 0, 'N');
 
-            rover.Command('B');
+                rover.Command('R');
 
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(-1, rover.Y);
-            Assert.AreEqual('N', rover.Heading);
-        }
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(0, rover.Y);
+                Assert.AreEqual('E', rover.Heading);
+            }
 
-        [Test]
-        public void Command_ForwardTwice_Valid()
-        {
-            var rover = new Rover(0, 0, 'N');
+            [Test]
+            public void Command_TurnLeft_Valid()
+            {
+                var rover = new Rover(0, 0, 'N');
 
-            rover.Command('F');
-            rover.Command('F');
+                rover.Command('L');
 
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(2, rover.Y);
-            Assert.AreEqual('N', rover.Heading);
-        }
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(0, rover.Y);
+                Assert.AreEqual('W', rover.Heading);
+            }
 
-        [Test]
-        public void Command_ForwardThenBackward_Valid()
-        {
-            var rover = new Rover(0, 0, 'N');
+            [Test]
+            public void Command_TurnRightTwice_Valid()
+            {
+                var rover = new Rover(0, 0, 'N');
 
-            rover.Command('F');
-            rover.Command('B');
+                rover.Command('R');
+                rover.Command('R');
 
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(0, rover.Y);
-            Assert.AreEqual('N', rover.Heading);
-        }
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(0, rover.Y);
+                Assert.AreEqual('S', rover.Heading);
+            }
 
-        [Test]
-        public void Command_Turn_Valid()
-        {
-            var rover = new Rover(0, 0, 'N');
+            [Test]
+            public void Command_TurnRightThenLeft_Valid()
+            {
+                var rover = new Rover(0, 0, 'N');
 
-            rover.Command('R');
+                rover.Command('R');
+                rover.Command('L');
 
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(0, rover.Y);
-            Assert.AreEqual('E', rover.Heading);
-        }
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(0, rover.Y);
+                Assert.AreEqual('N', rover.Heading);
+            }
 
-        [Test]
-        public void Command_TurnLeft_Valid()
-        {
-            var rover = new Rover(0, 0, 'N');
+            [Test]
+            public void Command_TurnRightFromEast_Valid()
+            {
+                var rover = new Rover(0, 0, 'E');
 
-            rover.Command('L');
+                rover.Command('R');
 
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(0, rover.Y);
-            Assert.AreEqual('W', rover.Heading);
-        }
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(0, rover.Y);
+                Assert.AreEqual('S', rover.Heading);
+            }
 
-        [Test]
-        public void Command_TurnRightTwice_Valid()
-        {
-            var rover = new Rover(0, 0, 'N');
+            [Test]
+            public void Command_TurnRightFromSouth_Valid()
+            {
+                var rover = new Rover(0, 0, 'S');
 
-            rover.Command('R');
-            rover.Command('R');
+                rover.Command('R');
 
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(0, rover.Y);
-            Assert.AreEqual('S', rover.Heading);
-        }
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(0, rover.Y);
+                Assert.AreEqual('W', rover.Heading);
+            }
 
-        [Test]
-        public void Command_TurnRightThenLeft_Valid()
-        {
-            var rover = new Rover(0, 0, 'N');
+            [Test]
+            public void Command_TurnRightFromWest_Valid()
+            {
+                var rover = new Rover(0, 0, 'W');
 
-            rover.Command('R');
-            rover.Command('L');
+                rover.Command('R');
 
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(0, rover.Y);
-            Assert.AreEqual('N', rover.Heading);
-        }
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(0, rover.Y);
+                Assert.AreEqual('N', rover.Heading);
+            }
 
-        [Test]
-        public void Command_TurnRightFromEast_Valid()
-        {
-            var rover = new Rover(0, 0, 'E');
+            [Test]
+            public void Command_TurnLeftFromWest_Valid()
+            {
+                var rover = new Rover(0, 0, 'W');
 
-            rover.Command('R');
+                rover.Command('L');
 
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(0, rover.Y);
-            Assert.AreEqual('S', rover.Heading);
-        }
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(0, rover.Y);
+                Assert.AreEqual('S', rover.Heading);
+            }
 
-        [Test]
-        public void Command_TurnRightFromSouth_Valid()
-        {
-            var rover = new Rover(0, 0, 'S');
+            [Test]
+            public void Command_TurnLeftFromSouth_Valid()
+            {
+                var rover = new Rover(0, 0, 'S');
 
-            rover.Command('R');
+                rover.Command('L');
 
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(0, rover.Y);
-            Assert.AreEqual('W', rover.Heading);
-        }
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(0, rover.Y);
+                Assert.AreEqual('E', rover.Heading);
+            }
 
-        [Test]
-        public void Command_TurnRightFromWest_Valid()
-        {
-            var rover = new Rover(0, 0, 'W');
+            [Test]
+            public void Command_TurnLeftFromEast_Valid()
+            {
+                var rover = new Rover(0, 0, 'E');
 
-            rover.Command('R');
+                rover.Command('L');
 
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(0, rover.Y);
-            Assert.AreEqual('N', rover.Heading);
-        }
-
-        [Test]
-        public void Command_TurnLeftFromWest_Valid()
-        {
-            var rover = new Rover(0, 0, 'W');
-
-            rover.Command('L');
-
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(0, rover.Y);
-            Assert.AreEqual('S', rover.Heading);
-        }
-
-        [Test]
-        public void Command_TurnLeftFromSouth_Valid()
-        {
-            var rover = new Rover(0, 0, 'S');
-
-            rover.Command('L');
-
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(0, rover.Y);
-            Assert.AreEqual('E', rover.Heading);
-        }
-
-        [Test]
-        public void Command_TurnLeftFromEast_Valid()
-        {
-            var rover = new Rover(0, 0, 'E');
-
-            rover.Command('L');
-
-            Assert.AreEqual(0, rover.X);
-            Assert.AreEqual(0, rover.Y);
-            Assert.AreEqual('N', rover.Heading);
+                Assert.AreEqual(0, rover.X);
+                Assert.AreEqual(0, rover.Y);
+                Assert.AreEqual('N', rover.Heading);
+            }
         }
     }
 }
