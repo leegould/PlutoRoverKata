d1320403c665ab7eac1a0da1ad090bee99e7d9d9
Lee <gould.lee@googlemail.com>
2016-01-28 21:50:12 +0000

    Rover class building

---
+++ PlutoRoverKata/Rover.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlutoRoverKata
+{
+    public class Rover
+    {
+        public Rover(int x, int y, char heading)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char Heading { get; set; }
+
+        public void Command(char cmd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
